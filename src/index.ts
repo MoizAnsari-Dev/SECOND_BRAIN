46d7eaa92b9f391a429d2a55b3c9f1951e4f1104
Moiz Ansari <moizansari.india@gmail.com>
2025-05-08 21:40:42 +0530

    Main file

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,39 @@
 import express from 'express';
+import mongooe from 'mongoose';
+import jwt from 'jsonwebtoken';
 
 const app = express();
+
+app.post('/user/signup', (req, res) => {
+    try {
+        res.send('Comming from signup')
+    } catch (error) {
+        res.status(400).send("Error: " + error)
+    }
+})
+app.post('/user/login', (req, res) => {
+    try {
+        res.send('Comming from login')
+    } catch (error) {
+        res.status(400).send("Error: " + error)
+    }
+})
+app.get('/user/contain', (req, res) => {
+    try {
+        res.send('Comming from Contains')
+    } catch (error) {
+        res.status(400).send("Error: " + error)
+    }
+})
+app.delete('/user/containdelete', (req, res) => {
+    try {
+        res.send('Comming from delete Contains')
+    } catch (error) {
+        res.status(400).send("Error: " + error)
+    }
+})
+
+
+app.listen(3000, () => {
+    console.log("Server is Live");
+})
